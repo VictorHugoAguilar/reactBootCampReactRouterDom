2f9e9a75771fcd6e13aa93f53c9fb05f4e3d42fe
victorHugoAguilar <victorhugoaguilar@aol.com>
2020-07-19 21:06:18 +0200

    Añadimos componentes para el ruteo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // Importamos los componentes personalizados
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
+import AllContact from './Components/AllContact/AllContact';
+import Error from './Components/Error/Error';
 import Home from './Components/Home/Home';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
@@ -14,11 +17,20 @@ function App() {
           <Route path="/about">
             <About />
           </Route>
-          <Route path="/contact">
+          <Route path="/contact/:id">
             <Contact />
+          </Route>
+          <Route path="/contact">
+            <AllContact />
           </Route>
           <Route path="/home">
             <Home />
+          </Route>
+          <Route path="/">
+            <Redirect to='/login' />
+          </Route>
+          <Route path="*">
+            <Error />
           </Route>
         </Switch>
       </BrowserRouter>
---
+++ src/Components/AllContact/AllContact.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AllContact = () => {
+    return (
+        <div><h2>All Contact</h2></div>
+    );
+}
+
+export default AllContact;
